81a9c727f9becd47323bce2eebf9e95f1638fdba
Rana Salem <ranatasalem@gmail.com>
2024-03-06 15:26:38 +0000

    fix typo in var name & bump version (patch)

--- app/insights.js
+++ app/insights.js
@@ -5,7 +5,7 @@ const setup = () => {
     appInsights.setup(process.env.APPINSIGHTS_CONNECTIONSTRING).start()
     console.log('App Insights running')
     const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
-    const appName = process.env.APPINSIGHTS-CLOUDROLE
+    const appName = process.env.APPINSIGHTS_CLOUDROLE
     appInsights.defaultClient.context.tags[cloudRoleTag] = appName
   } else {
     console.log('App Insights not running')
