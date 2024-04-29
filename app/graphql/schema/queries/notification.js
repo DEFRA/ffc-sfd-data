eafb927258749dd287f7c899c73267edc0e8584d
Fay Toward <fay.toward@defra.gov.uk>
2024-04-29 11:59:51 +0100

    Fix import path in notification query

--- app/graphql/schema/queries/notification.js
+++ app/graphql/schema/queries/notification.js
@@ -1,16 +1,17 @@
-const cosmos = require('../../cosmos')
+const cosmos = require('../../../cosmos')
 const { cosmosConfig } = require('../../../config')
 
-const notifications = async (_root, args, context) => {
+const notification = async (_root, args, context) => {
   const { messagesDatabase } = await cosmos()
-  const response = await messagesDatabase.container(cosmosConfig.messagesContainer).items.readAll().fetchAll()
+  const response = await messagesDatabase.container(cosmosConfig.messagesContainer).items.readAll().findOne()
   // update to return one notifcation only
   return {
     id: '',
     content: {}
   }
+  
 }
 
 module.exports = {
-  notifications
+  notification
 }
