0efedb3679575a99ede9f03e2afd3a908570c71e
John Watson <42574977+johnwatson484@users.noreply.github.com>
2024-05-28 12:54:03 +0100

    Support no cosmos (#11)

--- app/cosmos/init.js
+++ app/cosmos/init.js
@@ -1,21 +1,24 @@
 const fs = require('fs')
 const path = require('path')
+const { cosmosConfig } = require('../config')
 
 const initCosmos = async () => {
+  if (cosmosConfig.key) {
   // cosmos emulator is very slow to start up, if getting eccon refused suggest increase time to wait for cosmos to start
-  try {
-    const databasesDir = path.join(__dirname, './databases')
-    const files = fs.readdirSync(databasesDir)
+    try {
+      const databasesDir = path.join(__dirname, './databases')
+      const files = fs.readdirSync(databasesDir)
 
-    const createDatabases = files.map(async file => {
-      const databaseName = `${file.split('.')[0]}Database`
-      const db = require(`./databases/${file}`)
-      return db[databaseName]()
-    })
+      const createDatabases = files.map(async file => {
+        const databaseName = `${file.split('.')[0]}Database`
+        const db = require(`./databases/${file}`)
+        return db[databaseName]()
+      })
 
-    await Promise.all(createDatabases)
-  } catch (err) {
-    throw new Error(`Failed to create databases: ${err.message}`)
+      await Promise.all(createDatabases)
+    } catch (err) {
+      throw new Error(`Failed to create databases: ${err.message}`)
+    }
   }
 }
 
