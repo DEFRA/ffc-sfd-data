39eff10182aea0f7086302225a357f088f7c6919
Rana Salem <ranatasalem@gmail.com>
2024-06-03 13:24:22 +0100

    undo latest commit

--- app/cosmos/client.js
+++ app/cosmos/client.js
@@ -1,12 +1,6 @@
 const { CosmosClient } = require('@azure/cosmos')
 const { cosmosConfig } = require('../config')
 
-// if (cosmosConfig.key) {
-//   module.exports = new CosmosClient({ endpoint: cosmosConfig.endpoint, key: cosmosConfig.key })
-// }
-
 if (cosmosConfig.key) {
   module.exports = new CosmosClient({ endpoint: cosmosConfig.endpoint, key: cosmosConfig.key })
-} else {
-  module.exports = new CosmosClient({ endpoint: cosmosConfig.endpoint })
 }
