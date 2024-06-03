bb50e53f2c22c4999629b1e5d7e7587e72b2353d
Rana Salem <ranatasalem@gmail.com>
2024-06-03 13:17:47 +0100

    update cosmos client

--- app/cosmos/client.js
+++ app/cosmos/client.js
@@ -1,6 +1,12 @@
 const { CosmosClient } = require('@azure/cosmos')
 const { cosmosConfig } = require('../config')
 
+// if (cosmosConfig.key) {
+//   module.exports = new CosmosClient({ endpoint: cosmosConfig.endpoint, key: cosmosConfig.key })
+// }
+
 if (cosmosConfig.key) {
   module.exports = new CosmosClient({ endpoint: cosmosConfig.endpoint, key: cosmosConfig.key })
+} else {
+  module.exports = new CosmosClient({ endpoint: cosmosConfig.endpoint })
 }
