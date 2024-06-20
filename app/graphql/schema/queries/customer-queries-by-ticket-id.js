5ac2b3d25e63f8e1d8b8e6889bdec47ecbab1436
Rana Salem <ranatasalem@gmail.com>
2024-06-20 08:50:19 +0100

    add error handling for failed queries

--- app/graphql/schema/queries/customer-queries-by-ticket-id.js
+++ app/graphql/schema/queries/customer-queries-by-ticket-id.js
@@ -3,47 +3,59 @@ const { cosmosConfig } = require('../../../config')
 const { convertCosmosTimestamp } = require('../../../utils')
 
 const customerQueriesByTicketId = async (_root, args, context) => {
-  const { queriesDatabase } = await cosmos()
+  try {
+    const { queriesDatabase } = await cosmos()
 
-  const responseQuerySpec = {
-    query: 'SELECT * FROM customerQueryResponse cq WHERE cq.ticketId = @ticketId ORDER BY cq._ts DESC',
-    parameters: [{ name: '@ticketId', value: `${args.ticketId}` }]
-  }
+    const responseQuerySpec = {
+      query: 'SELECT * FROM customerQueryResponse cq WHERE cq.ticketId = @ticketId ORDER BY cq._ts DESC',
+      parameters: [{ name: '@ticketId', value: `${args.ticketId}` }]
+    }
 
-  const response = await queriesDatabase
-    .container(cosmosConfig.queriesContainer)
-    .items.query(responseQuerySpec)
-    .fetchAll()
+    const response = await queriesDatabase
+      .container(cosmosConfig.queriesContainer)
+      .items.query(responseQuerySpec)
+      .fetchAll()
 
-  const originalQuerySpec = {
-    query: 'SELECT * FROM customerQueryResponse cq WHERE cq.ticketId = @ticketId',
-    parameters: [{ name: '@ticketId', value: `${args.ticketId}` }]
-  }
+    if (!args.ticketId) {
+      throw new Error('ticketId is must be provided.')
+    }
 
-  const originalQueryResponse = await queriesDatabase
-    .container(cosmosConfig.queriesContainer)
-    .items.query(originalQuerySpec)
-    .fetchAll()
+    const originalQuerySpec = {
+      query: 'SELECT * FROM customerQueryResponse cq WHERE cq.ticketId = @ticketId',
+      parameters: [{ name: '@ticketId', value: `${args.ticketId}` }]
+    }
 
-  const originalQuery = originalQueryResponse.resources[0]
+    const originalQueryResponse = await queriesDatabase
+      .container(cosmosConfig.queriesContainer)
+      .items.query(originalQuerySpec)
+      .fetchAll()
 
-  return {
-    ticketId: args.ticketId,
-    _ts: convertCosmosTimestamp(originalQuery?._ts),
-    internalUser: originalQuery?.internalUser,
-    crn: originalQuery?.crn,
-    sbi: originalQuery?.sbi,
-    id: originalQuery?.id,
-    heading: originalQuery?.heading,
-    body: originalQuery?.body,
-    customerQueryResponses: response.resources.map((x) => ({
-      id: x.id,
-      ticketId: x.ticketId,
-      _ts: convertCosmosTimestamp(x._ts),
-      internalUser: x.internalUser,
-      heading: x.heading,
-      body: x.body
-    }))
+    if (!originalQueryResponse.resources.length) {
+      throw new Error(`No customer query data found for ticketId ${args.ticketId}`)
+    }
+
+    const originalQuery = originalQueryResponse.resources[0]
+
+    return {
+      ticketId: args.ticketId,
+      _ts: convertCosmosTimestamp(originalQuery?._ts),
+      internalUser: originalQuery?.internalUser,
+      crn: originalQuery?.crn,
+      sbi: originalQuery?.sbi,
+      id: originalQuery?.id,
+      heading: originalQuery?.heading,
+      body: originalQuery?.body,
+      customerQueryResponses: response.resources.map((x) => ({
+        id: x.id,
+        ticketId: x.ticketId,
+        _ts: convertCosmosTimestamp(x._ts),
+        internalUser: x.internalUser,
+        heading: x.heading,
+        body: x.body
+      }))
+    }
+  } catch (error) {
+    throw new Error(`Query failed: ${error.message}`)
   }
 }
 
--- app/graphql/schema/queries/customer-query-response.js
+++ app/graphql/schema/queries/customer-query-response.js
@@ -3,28 +3,40 @@ const { cosmosConfig } = require('../../../config')
 const { convertCosmosTimestamp } = require('../../../utils')
 
 const customerQueryResponse = async (_root, args, context) => {
-  const { queriesDatabase } = await cosmos()
+  try {
+    const { queriesDatabase } = await cosmos()
 
-  const querySpec = {
-    query: 'SELECT * FROM customerQueryResponse cq WHERE cq.id = @id',
-    parameters: [{ name: '@id', value: `${args.id}` }]
-  }
+    const querySpec = {
+      query: 'SELECT * FROM customerQueryResponse cq WHERE cq.id = @id',
+      parameters: [{ name: '@id', value: `${args.id}` }]
+    }
 
-  const response = await queriesDatabase
-    .container(cosmosConfig.queriesContainer)
-    .items.query(querySpec)
-    .fetchAll()
+    const response = await queriesDatabase
+      .container(cosmosConfig.queriesContainer)
+      .items.query(querySpec)
+      .fetchAll()
 
-  const resource = response.resources[0]
-  const ukTimestamp = resource ? convertCosmosTimestamp(resource._ts) : null
+    if (!args.id) {
+      throw new Error('id is must be provided.')
+    }
 
-  return {
-    id: response.resources[0]?.id,
-    ticketId: response.resources[0]?.ticketId,
-    _ts: ukTimestamp,
-    internalUser: response.resources[0]?.internalUser,
-    heading: response.resources[0]?.heading,
-    body: response.resources[0]?.body
+    if (!response.resources.length) {
+      throw new Error(`No customer query response data found for id ${args.id}`)
+    }
+
+    const resource = response.resources[0]
+    const ukTimestamp = resource ? convertCosmosTimestamp(resource._ts) : null
+
+    return {
+      id: response.resources[0]?.id,
+      ticketId: response.resources[0]?.ticketId,
+      _ts: ukTimestamp,
+      internalUser: response.resources[0]?.internalUser,
+      heading: response.resources[0]?.heading,
+      body: response.resources[0]?.body
+    }
+  } catch (error) {
+    throw new Error(`Query failed: ${error.message}`)
   }
 }
 
