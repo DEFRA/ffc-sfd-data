11c01242b2f3dd0c2a9a9c02b7b9d798e3067471
Rana Salem <ranatasalem@gmail.com>
2024-06-21 10:41:58 +0100

    update allCustomerQueryTickets to avoid duplication of the original query

--- app/graphql/schema/type-definitions.js
+++ app/graphql/schema/type-definitions.js
@@ -152,13 +152,13 @@ type CustomerQueriesByTicketId {
   message: String
   ticketId: String
   _ts: String
-  internalUser: Boolean
-  name: String
+  # internalUser: Boolean
+  # name: String
   crn: String
   sbi: String
-  id: String
-  heading: String
-  body: String
+  # id: String
+  # heading: String
+  # body: String
   customerQueryResponses: [CustomerQueryResponse]
 }
 
