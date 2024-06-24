2e9afc405697f0e6cf2fb7511403cdf4230adbef
Rana Salem <ranatasalem@gmail.com>
2024-06-24 19:10:27 +0100

    update error handling for update mutation

--- app/graphql/schema/mutations/update-customer-query-ticket.js
+++ app/graphql/schema/mutations/update-customer-query-ticket.js
@@ -6,6 +6,16 @@ const updateCustomerQueryTicket = async (_root, args, context) => {
   try {
     const { queriesDatabase } = await cosmos()
 
+    if (!args.ticketId) {
+      throw new Error('ticketId must be provided')
+    }
+
+    const customerQueryResponse = await customerQueryByTicketId(null, { ticketId: args.ticketId }, context)
+
+    if (!customerQueryResponse.customerQueryResponses.ticketId) {
+      throw new Error(`No customer query found for ticketId: ${args.ticketId}`)
+    }
+
     const item = {
       ticketId: args.ticketId,
       internalUser: args.internalUser,
@@ -18,17 +28,8 @@ const updateCustomerQueryTicket = async (_root, args, context) => {
       .container(cosmosConfig.queriesContainer)
       .items.upsert(item)
 
-    if (!args.ticketId) {
-      throw new Error('ticketId must be provided')
-    }
-
     const success = response.statusCode >= 200 && response.statusCode < 300
     const message = success ? 'Customer query ticket updated successfully with new response' : response.messages[0].message
-    const customerQueryResponse = await customerQueryByTicketId(null, { ticketId: args.ticketId }, context)
-
-    if (!customerQueryResponse.customerQueryResponses.ticketId) {
-      throw new Error(`No customer query found for ticketId: ${args.ticketId}`)
-    }
 
     return {
       code: response.statusCode,
