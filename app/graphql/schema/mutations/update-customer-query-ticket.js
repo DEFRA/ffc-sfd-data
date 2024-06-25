e363c5a004d89f20e1a8f05132e2851d521402a5
Rana Salem <ranatasalem@gmail.com>
2024-06-25 08:41:02 +0100

    update allCustomerQueryTickets

--- app/graphql/schema/mutations/update-customer-query-ticket.js
+++ app/graphql/schema/mutations/update-customer-query-ticket.js
@@ -3,46 +3,33 @@ const { cosmosConfig } = require('../../../config')
 const { customerQueryByTicketId } = require('../queries')
 
 const updateCustomerQueryTicket = async (_root, args, context) => {
-  try {
-    const { queriesDatabase } = await cosmos()
+  const { queriesDatabase } = await cosmos()
 
-    if (!args.ticketId) {
-      throw new Error('ticketId must be provided')
-    }
+  const item = {
+    ticketId: args.ticketId,
+    internalUser: args.internalUser,
+    name: args.name,
+    heading: args.heading,
+    body: args.body
+  }
 
-    const customerQueryResponse = await customerQueryByTicketId(null, { ticketId: args.ticketId }, context)
+  const response = await queriesDatabase
+    .container(cosmosConfig.queriesContainer)
+    .items.upsert(item)
 
-    if (!customerQueryResponse.customerQueryResponses.ticketId) {
-      throw new Error(`No customer query found for ticketId: ${args.ticketId}`)
-    }
+  const success = response.statusCode >= 200 && response.statusCode < 300
+  const message = success ? 'Customer query ticket updated successfully with new response' : response.messages[0].message
+  const customerQueryResponse = await customerQueryByTicketId(null, { ticketId: args.ticketId }, context)
 
-    const item = {
-      ticketId: args.ticketId,
-      internalUser: args.internalUser,
-      name: args.name,
-      heading: args.heading,
-      body: args.body
-    }
-
-    const response = await queriesDatabase
-      .container(cosmosConfig.queriesContainer)
-      .items.upsert(item)
-
-    const success = response.statusCode >= 200 && response.statusCode < 300
-    const message = success ? 'Customer query ticket updated successfully with new response' : response.messages[0].message
-
-    return {
-      code: response.statusCode,
-      success,
-      message,
-      ticketId: args.ticketId,
-      _ts: customerQueryResponse._ts,
-      crn: customerQueryResponse.crn,
-      sbi: customerQueryResponse.sbi,
-      customerQueryResponses: customerQueryResponse.customerQueryResponses
-    }
-  } catch (error) {
-    throw new Error(`Mutation failed: ${error.message}`)
+  return {
+    code: response.statusCode,
+    success,
+    message,
+    ticketId: args.ticketId,
+    _ts: customerQueryResponse._ts,
+    crn: customerQueryResponse.crn,
+    sbi: customerQueryResponse.sbi,
+    customerQueryResponses: customerQueryResponse.customerQueryResponses
   }
 }
 
--- app/graphql/schema/queries/all-customer-query-tickets.js
+++ app/graphql/schema/queries/all-customer-query-tickets.js
@@ -6,30 +6,69 @@ const allCustomerQueryTickets = async (_root, args, context) => {
   try {
     const { queriesDatabase } = await cosmos()
 
-    const querySpec = {
+    const responseQuerySpec = {
       query: 'SELECT * FROM customerQueryResponse cq ORDER BY cq._ts ASC'
     }
 
     const response = await queriesDatabase
       .container(cosmosConfig.queriesContainer)
-      .items.query(querySpec)
+      .items.query(responseQuerySpec)
       .fetchAll()
 
-    return {
-      originalCustomerQueryTickets: response.resources.map((x) => ({
+    const groupedResponses = response.resources.reduce((acc, x) => {
+      if (!acc[x.ticketId]) {
+        acc[x.ticketId] = {
+          ticketId: x.ticketId,
+          customerQueryResponses: []
+        }
+      }
+
+      acc[x.ticketId].customerQueryResponses.push({
         code: 200,
         success: true,
-        message: 'Query to Cosmos DB has been successful',
+        message: 'Query to Cosmos DB has been successful.',
+        id: x.id,
         ticketId: x.ticketId,
         _ts: convertCosmosTimestamp(x._ts),
         internalUser: x.internalUser,
         name: x.name,
-        crn: x.crn,
-        sbi: x.sbi,
-        id: x.id,
         heading: x.heading,
         body: x.body
-      }))
+      })
+      return acc
+    }, {})
+
+    const originalQueries = await Promise.all(Object.keys(groupedResponses).map(async (ticketId) => {
+      const originalQuerySpec = {
+        query: 'SELECT * FROM customerQueryResponse cq WHERE cq.ticketId = @ticketId',
+        parameters: [{ name: '@ticketId', value: ticketId }]
+      }
+
+      const originalQueryResponse = await queriesDatabase
+        .container(cosmosConfig.queriesContainer)
+        .items.query(originalQuerySpec)
+        .fetchAll()
+
+      const originalQuery = originalQueryResponse.resources[0]
+
+      return {
+        ...groupedResponses[ticketId],
+        code: 200,
+        success: true,
+        message: 'Query to Cosmos DB has been successful.',
+        _ts: convertCosmosTimestamp(originalQuery?._ts),
+        internalUser: originalQuery?.internalUser,
+        name: originalQuery?.name,
+        crn: originalQuery?.crn,
+        sbi: originalQuery?.sbi,
+        id: originalQuery?.id,
+        heading: originalQuery?.heading,
+        body: originalQuery?.body
+      }
+    }))
+
+    return {
+      originalCustomerQueryTickets: originalQueries
     }
   } catch (error) {
     throw new Error(`Query failed: ${error.message}`)
